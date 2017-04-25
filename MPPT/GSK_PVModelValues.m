f83062db0068e7717ea2255aa69abb22e19e938a
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-04-25 16:00:19 -0500

    Automatic push

---
+++ MPPT/GSK_PVModelValues.m
@@ -0,0 +1,13 @@
+% Constants taken from E20/435 SOLAR PANEL
+% file:///C:/Users/gadis/Downloads/ds-e20-series-435-solar-panel-datasheet.pdf
+T_n = 25+273;
+G_n = 1000;
+K_I = 3.5e-3; % Unit is Amps/Kelvin
+I_pv_n = 6.5; % Approximate value
+K_V = - 235.5e-3; % Unit is volts/Kelvin
+I_sc_n = 6.43;
+V_oc_n = 85.6;
+
+
+T = 273+40;
+V_t = N_s*k*T/q
