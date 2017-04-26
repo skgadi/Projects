6ed5d3081371187b1d5b00b1d11a1ca344e1ac02
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-04-26 18:55:20 -0500

    Automatic push

--- MPPT/GSK_PVModelValues.m
+++ MPPT/GSK_PVModelValues.m
@@ -1,5 +1,6 @@
 % Constants taken from E20/435 SOLAR PANEL
 % file:///C:/Users/gadis/Downloads/ds-e20-series-435-solar-panel-datasheet.pdf
+
 T_n = 25+273;
 G_n = 1000;
 K_I = 3.5e-3; % Unit is Amps/Kelvin
@@ -7,7 +8,16 @@ I_pv_n = 6.5; % Approximate value
 K_V = - 235.5e-3; % Unit is volts/Kelvin
 I_sc_n = 6.43;
 V_oc_n = 85.6;
+N_s = 128; % Number of cells connected in series
+
+a = 1.5; % Ideality factor
+
+R_s = 1e-3;
+R_p = 1000;
+
+k = physconst('Boltzmann');
+q = 1.60217662e-19;
 
 
 T = 273+40;
-V_t = N_s*k*T/q
+V_t = N_s*k*T/q;
