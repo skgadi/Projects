40b1d392a63f9388d78739191935382de6d80690
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-06-15 19:45:52 -0500

    Automatic push

---
+++ embeddedSystems/pic/programmer/code/progVer001.X/build/default/production/picLibs/app_led_usb_status.p1.d
@@ -0,0 +1,14 @@
+ build/default/production/picLibs/app_led_usb_status.d  \
+ build/default/production/picLibs/app_led_usb_status.p1:  \
+ picLibs/app_led_usb_status.c  \
+picLibs/io_mapping.h  \
+picLibs/usb_config.h  \
+picLibs/usb_device.h  \
+picLibs/usb_hal.h  \
+picLibs/usb_hal_pic18.h  \
+picLibs/usb_config.h  \
+picLibs/leds.h  \
+picLibs/buttons.h  \
+picLibs/system.h  \
+picLibs/usb_common.h  \
+picLibs/fixed_address_memory.h 
---
+++ embeddedSystems/pic/programmer/code/progVer001.X/main.h
@@ -0,0 +1,134 @@
+/* 
+ * File:   main.h
+ * Author: gadis
+ *
+ * Created on May 29, 2017, 10:18 AM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+/** CONFIGURATION Bits **********************************************/
+/*
+#pragma config CPUDIV = NOCLKDIV
+#pragma config USBDIV = OFF
+#pragma config FOSC   = XT
+#pragma config PLLEN  = ON
+#pragma config FCMEN  = OFF
+#pragma config IESO   = OFF
+#pragma config PWRTEN = OFF
+#pragma config BOREN  = OFF
+#pragma config BORV   = 30
+#pragma config WDTEN  = OFF
+#pragma config WDTPS  = 32768
+#pragma config MCLRE  = OFF
+#pragma config HFOFST = OFF
+#pragma config STVREN = ON
+#pragma config LVP    = ON
+#pragma config XINST  = OFF
+#pragma config BBSIZ  = OFF
+#pragma config CP0    = OFF
+#pragma config CP1    = OFF
+#pragma config CPB    = OFF
+#pragma config WRT0   = OFF
+#pragma config WRT1   = OFF
+#pragma config WRTB   = OFF
+#pragma config WRTC   = OFF
+#pragma config EBTR0  = OFF
+#pragma config EBTR1  = OFF
+#pragma config EBTRB  = OFF
+/**/
+/*********************************************************************
+
+ */
+    
+// PIC18F14K50 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1L
+#pragma config CPUDIV = NOCLKDIV// CPU System Clock Selection bits (No CPU System Clock divide)
+#pragma config USBDIV = OFF     // USB Clock Selection bit (USB clock comes directly from the OSC1/OSC2 oscillator block; no divide)
+
+// CONFIG1H
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config PLLEN = ON       // 4 X PLL Enable bit (Oscillator multiplied by 4)
+#pragma config PCLKEN = OFF     // Primary Clock Enable bit (Primary clock is under software control)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRTEN = OFF     // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 30        // Brown-out Reset Voltage bits (VBOR set to 3.0 V nominal)
+
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT is controlled by SWDTEN bit of the WDTCON register)
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config HFOFST = OFF     // HFINTOSC Fast Start-up bit (The system clock is held off until the HFINTOSC is stable.)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RA3 input pin enabled; MCLR disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config BBSIZ = OFF      // Boot Block Size Select bit (1kW boot block size)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Table Write Protection bit (Block 0 not write-protected)
+#pragma config WRT1 = OFF       // Table Write Protection bit (Block 1 not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+#define _XTAL_FREQ 12000000
+
+#include "system.h"
+
+#include "picLibs/app_device_cdc_basic.h"
+#include "picLibs/app_led_usb_status.h"
+
+#include "picLibs/usb.h"
+#include "picLibs/usb_device.h"
+#include "picLibs/usb_device_cdc.h"
+
+
+
+#include "libs/GenericTypeDefs.h"
+#include "libs/progPIC18F26K40.h"
+    
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
