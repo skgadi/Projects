2ce4d194964a05e829a79e5c64e690304e1520f4
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-06-27 13:18:04 -0500

    Automatic push

--- embeddedSystems/pic/programmer/code/progVer000.X/build/default/production/picLibs/leds.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/picLibs/leds.d  \
- build/default/production/picLibs/leds.p1:  \
- picLibs/leds.c  \
-picLibs/leds.h 
--- embeddedSystems/pic/programmer/code/progVer001.X/build/default/production/picLibs/buttons.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/picLibs/buttons.d  \
- build/default/production/picLibs/buttons.p1:  \
- picLibs/buttons.c  \
-picLibs/buttons.h 
